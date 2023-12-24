11b6943204c3ab2f97789fccd9a2449c2b2bea97
Chintan Boghara <140156149+chintanboghara@users.noreply.github.com>
2023-12-24 13:37:44 +0000

    Conversions & Operations of Data Types

--- javascript_practicals/p4.js
+++ javascript_practicals/p4.js
@@ -1,20 +1,56 @@
-let score = "21"
+// ******************************* Conversions *****************************
 
-console.log(typeof age);
-console.log(typeof (age));
+// let score = "21"
 
-let valueInNumber = Number(score)
-comsole.log(typeof valueInNumber);
-console.log(valueInNumber);
+// // console.log(typeof age);
+// // console.log(typeof (age));
 
-// "21" = 21
-// "21abc" = NaN
-// true = 1  false = 0 (Boolean)
-// "" = false 
-// "Chintan" = true
+// let valueInNumber = Number(score)
+// // comsole.log(typeof valueInNumber);
+// // console.log(valueInNumber);
 
-let someNumber = 21
+// // "21" = 21
+// // "21abc" = NaN
+// // true = 1  false = 0 (Boolean)
+// // "" = false 
+// // "Chintan" = true
 
-let stringNumber = string(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber)
+// let someNumber = 21
+
+// let stringNumber = string(someNumber)
+// // console.log(stringNumber);
+// // console.log(typeof stringNumber)
+
+// ****************************** Operations ****************************
+
+let value = 21
+let negValue = -value
+// console.log(negValue);
+
+// basic math operatios
+// console.log(1+1)
+// console.log (21**21)
+
+let str1 = 2
+let str2 = 3
+
+let str3 = str1 + str2
+// console.log(str3)
+
+// console.log("11" + 2);
+// console.log("11" + 2 + 2);
+// console.log(11 + 2 + "22")
+
+// console.log( (2 + 3) * 4 % 3) // write in specific parentheses
+
+// console.log(+true);
+// console.log(+"");
+
+// let num1, num2, num3
+
+// num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+gameCounter++;
+++gameCounter;
+console.log(gameCounter);
