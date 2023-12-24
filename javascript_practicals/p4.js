32f4c660ca0787aa9a30a88ee227871c9c298919
Chintan Boghara <140156149+chintanboghara@users.noreply.github.com>
2023-12-24 11:27:32 +0000

    Conversion Operation of Data Types

---
+++ javascript_practicals/p4.js
@@ -0,0 +1,20 @@
+let score = "21"
+
+console.log(typeof age);
+console.log(typeof (age));
+
+let valueInNumber = Number(score)
+comsole.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+// "21" = 21
+// "21abc" = NaN
+// true = 1  false = 0 (Boolean)
+// "" = false 
+// "Chintan" = true
+
+let someNumber = 21
+
+let stringNumber = string(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber)
