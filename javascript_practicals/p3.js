135070f0bb7d5147ef934e4f3d9cedefba3e186e
Chintan Boghara <140156149+chintanboghara@users.noreply.github.com>
2023-12-24 05:42:49 +0000

    Data Types

---
+++ javascript_practicals/p3.js
@@ -0,0 +1,21 @@
+"use strict"; /* treat all JS code as newer version */
+
+// alert(13+12) we are using nodejs not browser 
+
+// Datatypes in JS
+
+let name = "Chintan" // string
+let age = 21 // number
+let isLoggedIn = true //boolean 
+let state = null
+
+// bigint
+// Number
+// string
+// null = is object, standalone value, representation od empty value
+// undefined = value not defined
+// symbol
+
+// object
+
+console.log(typeof "Chintan"); // define type of datatype
